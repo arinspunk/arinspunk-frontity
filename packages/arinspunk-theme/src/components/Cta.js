4f166778739fac3c7d2b4b62f5944867b38053b9
Xúlio Zé <arinspunk@gmail.com>
2021-12-05 01:39:16 +0100

    Cta
    Add Cta component

---
+++ packages/arinspunk-theme/src/components/Cta.js
@@ -0,0 +1,23 @@
+// File: /packages/my-first-theme/src/components/intro.js
+import React from "react";
+import { connect, css } from "frontity";
+
+const cta = css`
+    padding: 10px 40px;
+    border: 1px solid #fff;
+    border-radius: 40px;
+    text-decoration: none;
+    color: #fff;
+    &:hover {
+        background-color: #fff;
+        color: #000;
+    }
+`;
+
+const Cta = ({ href, children }) => (
+    <a href={href} css={cta}>
+        {children}
+    </a>
+);
+
+export default connect(Cta);
--- packages/arinspunk-theme/src/components/intro.js
+++ packages/arinspunk-theme/src/components/intro.js
@@ -1,19 +1,33 @@
 // File: /packages/my-first-theme/src/components/intro.js
 import React from "react";
 import { connect } from "frontity";
-import styles from './Intro.module.css'; 
+import { styled } from "frontity";
+import Cta from "./Cta";
 
 const Intro = (props) => {
     return (
-        <div className={styles.intro}>
-            <h1 className={styles}>
-                {props.title}
-            </h1>
-            <div className={styles['intro__content']}>
-                {props.content}
-            </div>
-        </div>
+        <>
+            <IntroWrap>
+                <IntroTitle>
+                    {props.title}
+                </IntroTitle>
+                <IntroContent>
+                    {props.content}
+                </IntroContent>
+            </IntroWrap>
+            <Cta href="http://google.es" children="Uma ligação"></Cta>
+        </>
     );
 }
+
+const IntroWrap = styled.div`
+    margin-bottom: 48px;
+`;
+const IntroTitle = styled.h1`
+    margin-bottom: 16px;
+`;
+const IntroContent = styled.div`
+    margin-bottom: 0px;
+`;
 
 export default connect(Intro);
